3ddc945372d397c9fe9760632bca43ee9b7f76c4
hakan <imparatorahmett@gmail.com>
2022-07-09 01:14:37 +0300

    fix redirects

--- src/Pages/Login/password-reset.js
+++ src/Pages/Login/password-reset.js
@@ -4,19 +4,14 @@ import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert"
 import Form from "../../Components/Form";
 
-import { useCallback, useState, useRef, useEffect } from "react";
+import { useCallback, useState, useRef } from "react";
 import { supabase } from '../../supabaseClient';
 
 function PasswordReset() {
   const style = {sx: {mt: 2}};
   const [message, setMessage] = useState(null);
-  const [hash, setHash] = useState(null);
   const passwordRef = useRef(null);
   const rePasswordRef = useRef(null);
-
-  useEffect(() => { 
-    setHash(window.location.hash);
-  }, []);
 
   const resetPassword = useCallback(async () => {
     setMessage(["info", "Resetting Password"]);
@@ -27,27 +22,8 @@ function PasswordReset() {
       setMessage(["error", "Passwords do not match."]); 
       return;
     }
-    const hashArr = hash
-      .substring(1)
-      .split("&")
-      .map((param) => param.split("="));
 
-    let type;
-    let accessToken;
-    for (const [key, value] of hashArr) {
-      if (key === "type") {
-        type = value;
-      } else if (key === "access_token") {
-        accessToken = value;
-      }
-    }
-
-    if (type !== "recovery" || !accessToken || typeof accessToken === "object") {
-      setMessage(["error", "Invalid access token."]); 
-      return;
-    }
-
-    const { error } = await supabase.auth.api.updateUser(accessToken, {
+    const { error } = await supabase.auth.update({
       password: password,
     });
 
@@ -55,8 +31,8 @@ function PasswordReset() {
       setMessage(["error", error.message]); 
       return;
     }
-    setMessage(["success", "Check your mails!"]);
-  }, [hash]);
+    setMessage(["success", "Password reset!"]);
+  }, []);
 
   const handleEnter = useCallback(event => {
     if (event.key === "Enter") resetPassword();
@@ -68,7 +44,7 @@ function PasswordReset() {
     <Form>
       <Typography color="primary" variant="h3">Password Reset</Typography>
       <TextField error={isError} {...style} fullWidth variant="outlined" label="Password" type="password" inputRef={passwordRef} onKeyUp={handleEnter} />
-      <TextField error={isError} {...style} fullWidth variant="outlined" label="Password Check" type="password" inputRef={rePasswordRef} onKeyUp={resetPassword} />
+      <TextField error={isError} {...style} fullWidth variant="outlined" label="Password Check" type="password" inputRef={rePasswordRef} onKeyUp={handleEnter} />
 
       { message && <Alert {...style} severity={message[0]}>{message[1]}</Alert>}
       <Button {...style} aria-label="log in" variant="contained" onClick={resetPassword}>Change Password</Button>
