163ebd2426084a6830dc6a4716edc7c164cfb88f
hakan <hakancandar@protonmail.com>
2023-08-23 21:49:56 +0300

    improve profile settings page

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,4 +1,4 @@
-import { Alert, Button, LinearProgress, Paper, TextField, Typography } from "@mui/material";
+import { Alert, Avatar, Button, LinearProgress, Paper, TextField, Typography } from "@mui/material";
 import DoneIcon from '@mui/icons-material/Done';
 import { Box } from "@mui/system";
 import { memo, useEffect, useState } from "react";
@@ -31,6 +31,7 @@ function Profile() {
     const file = event.target.files[0];
     const { name } = file;
     setFilename([name, 0]);
+    setCleared(false);
 
     const userId = supabase.auth.user().id;
 
@@ -77,22 +78,31 @@ function Profile() {
       </Paper> 
     );
   }
+
+  let currPhotoUnsaved = updatedProfile?.avatar_url || profile.avatar_url;
+  if (updatedProfile?.avatar_url === "") currPhotoUnsaved = "";
+
   return (
     <Paper sx={{my: 2, p: 2}}>
       <Typography variant="h5" color="primary">Profile Settings</Typography>
+      <Box sx={{my: 2, mb: 3, display: "flex", alignItems: "center", gap: 3}}>
+        <Avatar src={currPhotoUnsaved && supabase.storage.from("avatars").getPublicUrl(currPhotoUnsaved).publicURL} sx={{ width: 90, height: 90 }}/>
+        <Box sx={{flexGrow: 1}}>
+          <Box sx={{display: "flex"}}>
+            <Button fullWidth sx={{maxWidth: "150px"}} variant="contained" component="label">
+              Upload Image
+              <input type="file" accept="image/png" hidden onChange={imageChange} />
+            </Button>
+            { filename && <Typography sx={{alignSelf: "center", ml: 1}}>{filename[0]} {filename[1] ? <DoneIcon color="primary" sx={{verticalAlign: "middle"}}/> : null}</Typography> }
+          </Box>
+          { filename && !filename[1] && <LinearProgress sx={{mt: 1, maxWidth: "225px"}} />}
+          <Box sx={{mt: 1, display: "flex"}}>
+            <Button fullWidth variant="contained" sx={{maxWidth: "150px"}} onClick={clearImage}>Clear Image</Button>
+            { cleared && <Typography sx={{alignSelf: "center", ml: 1}}><DoneIcon color="primary" sx={{verticalAlign: "middle"}}/></Typography> }
+          </Box>
+        </Box>
+      </Box>
       <TextField {...style} label="Username" variant="outlined" defaultValue={profile.username} onKeyUp={event => valueChange("username", event)} />
-      <Box sx={{mt: 1, display: "flex"}}>
-        <Button fullWidth sx={{maxWidth: "150px"}} variant="contained" component="label">
-          Upload Image
-          <input type="file" accept="image/png" hidden onChange={imageChange} />
-        </Button>
-        { filename && <Typography sx={{alignSelf: "center", ml: 1}}>{filename[0]} {filename[1] ? <DoneIcon color="primary" sx={{verticalAlign: "middle"}}/> : null}</Typography> }
-      </Box>
-      { filename && !filename[1] && <LinearProgress sx={{mt: 1, maxWidth: "225px"}} />}
-      <Box sx={{mt: 1, display: "flex"}}>
-        <Button fullWidth variant="contained" sx={{maxWidth: "150px"}} onClick={clearImage}>Clear Image</Button>
-        { cleared && <Typography sx={{alignSelf: "center", ml: 1}}><DoneIcon color="primary" sx={{verticalAlign: "middle"}}/></Typography> }
-      </Box>
       <TextField {...style} label="E-mail" variant="outlined" value={profile.email} disabled />
       { error && <Alert sx={{my: 2}} severity="error">{error}</Alert>}
       <Box sx={{maxWidth: "150px"}}>
